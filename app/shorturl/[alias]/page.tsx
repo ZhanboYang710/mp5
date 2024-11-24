49a4b61a7ff00375f3a4858d77c093c28757491e
ZhanboYang710 <zyang710@bu.edu>
2024-11-24 01:41:28 -0500

    im losing it

--- app/shorturl/[alias]/page.tsx
+++ app/shorturl/[alias]/page.tsx
@@ -1,10 +1,8 @@
-// "use server";
 import getAlias from "@/lib/getAlias";
 import { redirect } from "next/navigation";
 
-
 export default async function showShortUrlSite(
-    { params, } : {params: {alias: string} }
+    { params } : {params: {alias: string} }
 ) {
     console.log("Alias received in route:", params.alias);
     const url_obj = await getAlias(params.alias);
