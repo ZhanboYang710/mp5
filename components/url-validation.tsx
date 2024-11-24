4fb2cb1e4d9f902d1487d59d2538292ae9a63dc5
ZhanboYang710 <zyang710@bu.edu>
2024-11-24 01:13:17 -0500

    more debugging zzZ

--- components/url-validation.tsx
+++ components/url-validation.tsx
@@ -4,7 +4,7 @@ export default function checkUrl(input_url: string): boolean {
     try {
         new URL(input_url);
         return true;
-    } catch (e) {
+    } catch (_e) {
         return false;
     }
 }
