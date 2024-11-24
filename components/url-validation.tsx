5b7984122d1a8fd8bb94c750e8379fcf18bec39a
ZhanboYang710 <zyang710@bu.edu>
2024-11-24 01:19:43 -0500

    am I dumb?

--- components/url-validation.tsx
+++ components/url-validation.tsx
@@ -1,10 +1,10 @@
 
-
 export default function checkUrl(input_url: string): boolean {
     try {
         new URL(input_url);
         return true;
-    } catch (_e) {
+    } catch (error) {
+        console.error("Invalid URL:", error);
         return false;
     }
 }
